9d1d50256f22c24e0c59c451bb0a9ff4f45d7a9b
johand2021-x <90793798+GRUPO429Cilco3@users.noreply.github.com>
2021-10-09 19:10:52 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    NavBarComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
