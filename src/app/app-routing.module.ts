f906ae9056832c8683c59fc9370ceb3b84cfdf3f
johand2021-x <90793798+GRUPO429Cilco3@users.noreply.github.com>
2021-10-09 21:30:42 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelpComponent } from './home/help/help.component';
 import { HomeComponent } from './home/home.component';
+import { InitComponent } from './home/init/init.component';
+import { SettingComponent } from './home/setting/setting.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:"Home",component:HomeComponent},
+  {path:"Home",component:HomeComponent ,children:[{path:"Init",component:InitComponent},
+  {path:"Setting",component:SettingComponent},
+  {path:"Help",component:HelpComponent}]},
   {path:"Login",component:LoginComponent},
   {path: "**", redirectTo:"Login"}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SettingComponent } from './home/setting/setting.component';
+import { HelpComponent } from './home/help/help.component';
+import { InitComponent } from './home/init/init.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SettingComponent,
+    HelpComponent,
+    InitComponent
   ],
   imports: [
     BrowserModule,
